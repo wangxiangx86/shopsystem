4025596a9d7a7222db96a8a0f83d832a0e2d9c12
wangxiangx86 <40423042+wangxiangx86@users.noreply.github.com>
2018-06-20 16:41:11 +0800

    sql文件

---
+++ goods.sql
@@ -0,0 +1,7 @@
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (1, '饼干', 3, 10, 20);
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (2, '雪碧', 3, 3.5, 10);
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (3, '可乐', 3, 3.5, 20);
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (5, 'iPhone7', 4, 5499, 30);
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (6, '1加5', 4, 3999, 40);
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (7, '佳能m5', 4, 5488, 50);
+INSERT INTO `goods` (`id`, `goods_name`, `typeid`, `goods_price`, `goods_num`) VALUES (8, '富士X-2', 4, 6400, 60);
---
+++ goodstype.sql
@@ -0,0 +1,2 @@
+INSERT INTO `goodstype` (`typeid`, `typename`) VALUES (3, '食品');
+INSERT INTO `goodstype` (`typeid`, `typename`) VALUES (4, '电子产品');
